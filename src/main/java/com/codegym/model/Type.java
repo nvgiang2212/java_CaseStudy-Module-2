8cc9b01cafb0d1dc9371688187a1c633cabe830b
vgiang <Giang221295>
2019-07-17 10:08:33 +0700

    add model type

---
+++ src/main/java/com/codegym/model/Type.java
@@ -0,0 +1,39 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "types")
+public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Type() {
+
+    }
+
+    public Type(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
